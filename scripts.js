2df9f889076a0ce9ebfc065c411eaa4ae9d26a14
Dallas Gale <gale.dallas@abc.net.au>
2022-07-29 19:22:31 +1000

    wip

--- scripts.js
+++ scripts.js
@@ -1,43 +1,24 @@
-// const buyCta = document.getElementById('cta-buy')
-// const buyContainer = document.querySelector('.buy-container')
-// buyCta.addEventListener('mouseenter', () => {
-//     console.log('open', buyContainer)
-//     buyContainer.classList.add('open')
-// })
-// buyCta.addEventListener('mouseleave', () => {
-//     console.log('close')
-//     buyContainer.classList.remove('open')
-// })
-
-// const listenCta = document.getElementById('cta-listen')
-// const listenContainer = document.querySelector('.listen-container')
-// listenCta.addEventListener('mouseenter', () => {
-//     console.log('open', buyContainer)
-//     listenContainer.classList.add('open')
-// })
-// listenCta.addEventListener('mouseleave', () => {
-//     console.log('close')
-//     listenContainer.classList.remove('open')
-// })
-
-const readMoreBtn = document.querySelector('.read-more-btn')
+const moreInfoCta = document.querySelector('#info-cta')
 const readMoreContainer = document.querySelector('.read-more')
-const readMoreBtnText = document.querySelector('.read-more-btn-text')
+const moreInfoCtaText = document.querySelector('.read-more-btn-text')
+const buyCta = document.querySelector('#buy-cta')
+const buyContainer = document.querySelector('#buy-container')
 
-readMoreBtn.addEventListener('click', () => {
+moreInfoCta.addEventListener('click', () => {
     readMoreContainer.classList.toggle('open')
     setTimeout(() => {
-        readMoreBtnText.textContent = 'Read more'
-        if (readMoreContainer.classList.contains('open')) {
-            readMoreBtnText.textContent = 'Read less'
+        if (buyContainer.classList.contains('show')) {
+            console.log('contains show')
+            buyContainer.classList.remove('show')
+            buyContainer.classList.add('hide')
         }
     }, 0)
 })
 
-const epImg = document.querySelector('.ep-artwork')
-const footer = document.querySelector('.footer')
-
-epImg.addEventListener('mouseover', () => {
-    footer.classList.remove('hide')
-    footer.classList.add('show')
+buyCta.addEventListener('click', () => {
+    buyContainer.classList.remove('hide')
+    buyContainer.classList.add('show')
+    if (readMoreContainer.classList.contains('open')) {
+        readMoreContainer.classList.remove('open')
+    }
 })
